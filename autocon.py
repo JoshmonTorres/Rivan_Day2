7509185715d248bf6edbc335f62b929423f236de
ArJames Inson <aerin.james820@gmail.com>
2026-05-17 00:59:31 +0800

    Update autocon.py

--- autocon.py
+++ autocon.py
@@ -66,18 +66,13 @@ elif lab == "3":
     connections = [
         "/TELNET " + rst_ip + " 2001",
         "/TELNET " + rst_ip + " 2002",
+        "/TELNET " + rst_ip + " 2003",
+        "/TELNET " + rst_ip + " 2004",
         "/TELNET " + rst_ip + " 2005",
         "/TELNET " + rst_ip + " 2006",
         "/TELNET " + rst_ip + " 2007",
         "/TELNET " + rst_ip + " 2008",
-        "/TELNET " + rst_ip + " 2009",
-        "/TELNET " + rst_ip + " 2010",
-        "/TELNET " + rst_ip + " 2011",
-        "/TELNET " + rst_ip + " 2012",
-        "/TELNET " + rst_ip + " 2013",
-        "/TELNET " + rst_ip + " 2014",
-        "/TELNET " + rst_ip + " 2015",
-        "/TELNET " + rst_ip + " 2016"
+        "/TELNET " + rst_ip + " 2009"
     ]
 elif lab == "4":
     connections = [
